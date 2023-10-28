58ff041e96ac40d4dce716194a6573e1715cedf0
mojad <Salam995996>
2023-10-28 17:17:10 -0400

    volcanic-field-252_automated_commit

--- model.py
+++ model.py
@@ -195,7 +195,7 @@ def train_generalized_CNN():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     batch_size = 10000
     lr = 5e-3
-    max_epoch = 15
+    max_epoch = 100
     wandb.init(project='generalized CNN', mode='online')
     wandb.config = {"learning_rate": lr, "epochs": max_epoch, "batch_size": batch_size}
     if wandb.run.name is None:
