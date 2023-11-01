91d5c34abf316f6e950de963915e276ee86b828e
mojad <Salam995996>
2023-10-31 22:16:14 -0400

    wild-sweep-1_automated_commit

--- model.py
+++ model.py
@@ -330,7 +330,7 @@ def train_generalized_CNN():
     test_data = DataLoader(test_dataset, batch_size=batch_size)
 
     hmc = homemade_cnn(batch_size=batch_size, device=device).to(device)
-    hmc = torch.load("NN_model/distinctive-snowflake-255model.trc")
+    # hmc = torch.load("NN_model/distinctive-snowflake-255model.trc")
     opt = AdamW(hmc.parameters(), lr=lr)
     lambda1 = lambda epoch: 0.99 ** epoch
     scheduler = torch.optim.lr_scheduler.LambdaLR(opt, lr_lambda=lambda1)
