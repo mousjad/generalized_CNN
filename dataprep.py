21115944448b21cd1d22e9ee808d01480b17341f
mojad <Salam995996>
2023-11-04 06:23:42 -0400

    tough-night-88_automated_commit

--- dataprep.py
+++ dataprep.py
@@ -21,7 +21,7 @@ def create_scan_dist(dir_id="scan_data/", mode='train'):
     fid_dict = {'test': '_TEST', 'train': 'TRAIN'}
 
     for subdir_id in tqdm(os.listdir(dir_id)):
-        if subdir_id[-5:] == fid_dict[mode]:
+        if subdir_id[-5:] == fid_dict[mode] and subdir_id[0] != ".":
             ref_mesh_fid = "cad_model/" + subdir_id.split('.')[0] + ".stl"
             ref_mesh = trimesh.load(ref_mesh_fid)
             subdir_id = dir_id + subdir_id + "/"
@@ -91,41 +91,117 @@ def create_conv_data(master_scan_dist_list, master_ref_mesh_list):
 
     return master_conv
 
-def create_conv_image_indices(ref_mesh, shape=15, step=0.75, f_id = 'p.pkl'):
+def create_conv_image_indices(ref_mesh_heavy, ref_mesh_light, shape=15, step=0.75, f_id = 'p.pkl'):
     # Calculate sharp edges and other values
-    k_tree = ref_mesh.kdtree
-    SE, _, _, _, _, _ = igl.sharp_edges(ref_mesh.vertices, ref_mesh.faces, 1.4)
 
-    # Divide vertices into chunks for parallel processing
-    num_vertices = ref_mesh.vertices.shape[0]
-    chunk_size = num_vertices // mp.cpu_count()
-    vertex_chunks = [
-        ref_mesh.vertices[i:i + chunk_size, :]
-        for i in range(0, num_vertices, chunk_size)
-    ]
     # === multiprocessed ===
     # print('Starting pool of processes')
     # # Create a pool of processes
-    # with mp.Pool(processes=1) as pool:
-    #     # Map the compute_indices function over the vertex chunks in parallel, passing
-    #     # the necessary arguments to the function as a tuple
-    #     results = pool.starmap(
-    #         compute_indices,
-    #         [(chunk, ref_mesh, shape, step, SE, k_tree) for chunk in vertex_chunks]
-    #     )
-    #
+    # with Pool() as pool:
+    #     all_scan_case_dist = pool.map(MP_compute_indices,
+    #                                    [(i, vert, ref_mesh, shape,
+    #                                      step, SE, k_tree) for i, vert in enumerate(ref_mesh.vertices)])
+
     # # Combine the results from each process
     # all_scan_case_dist = [item for sublist in results for item in sublist]
 
     # === Single process ===
-    all_scan_case_dist = compute_indices(ref_mesh, shape, step, SE, k_tree)
+    # all_scan_case_dist = compute_indices(ref_mesh, shape, step, SE, k_tree)
 
     # Save the results to a file and return them
+
+    all_scan_case_dist = alternate_compute_indices(ref_mesh_light, ref_mesh_heavy, 15, 0.1)
+
     if f_id is not None:
         with open(f_id, 'wb') as f:
             pickle.dump(all_scan_case_dist, f)
             print('saved')
     return all_scan_case_dist
+
+def alternate_compute_indices(ref_mesh_light, ref_mesh_heavy, shape, step):
+    k_tree = ref_mesh_heavy.kdtree
+    SE, _, _, _, _, _ = igl.sharp_edges(ref_mesh_heavy.vertices, ref_mesh_heavy.faces, 1.4)
+    all_scan_case_dist = []
+    for i in tqdm(range(ref_mesh_light.vertices.shape[0])):
+        vi = ref_mesh_light.vertices[i]
+        ni = ref_mesh_light.vertex_normals[i]
+
+        if np.abs(ni).argmax() == 2:
+            ori = vi - np.array((0, 0, vi[2]))
+            if np.all(ori == np.zeros(3)):
+                ori = np.array((1, 0, 0))
+            else:
+                ori = ori / np.linalg.norm(ori)
+        else:
+            ori = np.array((0, 0, 1))
+
+        peri = np.cross(ni, ori)
+        ori = np.cross(peri, ni)
+
+        X, Y = [], []
+        for s in np.linspace(-(shape - 1) / 2 * step, (shape - 1) / 2 * step, shape):
+            X.append(peri * s)
+            Y.append(ori * s)
+        X = np.array(X)
+        Y = np.array(Y)
+
+        scan_case_dist = []
+        for u1, x in enumerate(X):
+            for v1, y in enumerate(Y):
+                v = vi - x - y
+                p = k_tree.query_ball_point(v, step)
+                if len(p) != 0:
+                    p = list(set(p) - set(list(np.unique(SE))))
+                    p = np.asarray(p)[np.nonzero(ref_mesh_heavy.vertex_normals[p, :].dot(ni) > 0.67)]
+                if len(p) == 0:
+                    scan_case_dist.append(-1)
+                else:
+                    scan_case_dist.append(p)
+
+        all_scan_case_dist.append(scan_case_dist)
+
+    # Return list of computed indices for all vertices in the chunk
+    return all_scan_case_dist
+
+
+def MP_compute_indices(args):
+    i, vert, ref_mesh, shape, step, SE, k_tree = args
+    ni = ref_mesh.vertex_normals[i, :]
+
+    if np.abs(ni).argmax() == 2:
+        ori = vert - np.array((0, 0, vert[2]))
+        if np.all(ori == np.zeros(3)):
+            ori = np.array((1, 0, 0))
+        else:
+            ori = ori / np.linalg.norm(ori)
+    else:
+        ori = np.array((0, 0, 1))
+
+    peri = np.cross(ni, ori)
+    ori = np.cross(peri, ni)
+
+    X, Y = [], []
+    for s in np.linspace(-(shape - 1) / 2 * step, (shape - 1) / 2 * step, shape):
+        X.append(peri * s)
+        Y.append(ori * s)
+    X = np.array(X)
+    Y = np.array(Y)
+
+    scan_case_dist = []
+    for u1, x in enumerate(X):
+        for v1, y in enumerate(Y):
+            v = vert - x - y
+            p = k_tree.query_ball_point(v, step)
+            if len(p) != 0:
+                p = list(set(p) - set(list(np.unique(SE))))
+                p = np.asarray(p)[np.nonzero(ref_mesh.vertex_normals[p, :].dot(ni) > 0.67)]
+            if len(p) == 0:
+                scan_case_dist.append(-1)
+            else:
+                scan_case_dist.append(p)
+
+    return [scan_case_dist, i]
+
 
 def compute_indices(ref_mesh, shape, step, SE, k_tree):
     all_scan_case_dist = []
@@ -203,9 +279,10 @@ if __name__ == '__main__':
     # fid = 'cad_model/mod_nist_light_in_process.stl'
     # p_fid = 'cad_indices/' + fid.split('/')[1].split('.')[0] + '.pkl'
     # p = create_conv_image_indices(trimesh.load(fid), 15, 0.75, p_fid)
-    # fid = 'cad_model/mod_nist_light.stl'
-    # p_fid = 'cad_indices/' + fid.split('/')[1].split('.')[0] + '.pkl'
-    # p = create_conv_image_indices(trimesh.load(fid), 15, 0.75, p_fid)
+    # fid_light = 'cad_model/mod_nist_light.stl'
+    # fid_heavy = 'cad_model/mod_nist.stl'
+    # p_fid = 'cad_indices/' + fid_heavy.split('/')[1].split('.')[0] + '.pkl'
+    # p = create_conv_image_indices(trimesh.load(fid_heavy), trimesh.load(fid_light), 15, 0.75, p_fid)
     # fid = 'cad_model/big_test_part_1_light.stl'
     # p_fid = 'cad_indices/' + fid.split('/')[1].split('.')[0] + '.pkl'
     # p = create_conv_image_indices(trimesh.load(fid), 15, 0.75, p_fid)
@@ -221,23 +298,23 @@ if __name__ == '__main__':
     # fid = 'cad_model/test_part_5_light.stl'
     # p_fid = 'cad_indices/' + fid.split('/')[1].split('.')[0] + '.pkl'
     # p = create_conv_image_indices(trimesh.load(fid), 15, 0.75, p_fid)
-    fid = 'cad_model/test_part_6_light.stl'
-    p_fid = 'cad_indices/' + fid.split('/')[1].split('.')[0] + '.pkl'
-    p = create_conv_image_indices(trimesh.load(fid), 15, 0.75, p_fid)
+    # fid = 'cad_model/test_part_6_light.stl'
+    # p_fid = 'cad_indices/' + fid.split('/')[1].split('.')[0] + '.pkl'
+    # p = create_conv_image_indices(trimesh.load(fid), 15, 0.75, p_fid)
 
     print('indices done')
     # raise Exception('done')
 
     # === Train dataprep ===
     print("started train dataprep\n")
-    # master_scan_dist_list, master_ave_dist_list, master_ref_mesh_list = create_scan_dist(mode='train')
-    #
-    # with open('temp/master_scan_dist_list.pkl', 'wb') as f:
-    #     pickle.dump(master_scan_dist_list, f)
-    # with open('temp/master_ave_dist_list.pkl', 'wb') as f:
-    #     pickle.dump(master_ave_dist_list, f)
-    # with open('temp/master_ref_mesh_list.pkl', 'wb') as f:
-    #     pickle.dump(master_ref_mesh_list, f)
+    master_scan_dist_list, master_ave_dist_list, master_ref_mesh_list = create_scan_dist(mode='train')
+
+    with open('temp/master_scan_dist_list.pkl', 'wb') as f:
+        pickle.dump(master_scan_dist_list, f)
+    with open('temp/master_ave_dist_list.pkl', 'wb') as f:
+        pickle.dump(master_ave_dist_list, f)
+    with open('temp/master_ref_mesh_list.pkl', 'wb') as f:
+        pickle.dump(master_ref_mesh_list, f)
 
 
     with open('temp/master_scan_dist_list.pkl', 'rb') as f:
@@ -255,15 +332,15 @@ if __name__ == '__main__':
 
 
     # === Test dataprep ===
-    # print("Started test dataprep\n")
-    # master_scan_dist_list, master_ave_dist_list, master_ref_mesh_list = create_scan_dist(mode='test')
-    #
-    # with open('temp/test_master_scan_dist_list.pkl', 'wb') as f:
-    #     pickle.dump(master_scan_dist_list, f)
-    # with open('temp/test_master_ave_dist_list.pkl', 'wb') as f:
-    #     pickle.dump(master_ave_dist_list, f)
-    # with open('temp/test_master_ref_mesh_list.pkl', 'wb') as f:
-    #     pickle.dump(master_ref_mesh_list, f)
+    print("Started test dataprep\n")
+    master_scan_dist_list, master_ave_dist_list, master_ref_mesh_list = create_scan_dist(mode='test')
+
+    with open('temp/test_master_scan_dist_list.pkl', 'wb') as f:
+        pickle.dump(master_scan_dist_list, f)
+    with open('temp/test_master_ave_dist_list.pkl', 'wb') as f:
+        pickle.dump(master_ave_dist_list, f)
+    with open('temp/test_master_ref_mesh_list.pkl', 'wb') as f:
+        pickle.dump(master_ref_mesh_list, f)
 
 
     with open('temp/test_master_scan_dist_list.pkl', 'rb') as f:
