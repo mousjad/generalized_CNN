da73b6ebba25877b934756fbae5daef4b5a60d42
mojad <Salam995996>
2023-11-09 07:19:53 -0500

    royal-spaceship-7_automated_commit

--- model_MLP.py
+++ model_MLP.py
@@ -78,7 +78,7 @@ class homemade_cnn(Module):
         # y = y * mask
         # y = torch.flatten(self.drop8(self.r8(self.c8(y))), start_dim=1)
         # y = self.Lin1(y)
-        y = torch.cat((y, input2[:, None]), 1)
+        y = torch.cat((torch.flatten(y, start_dim=1), input2[:, None]), 1)
         y = self.lr1(self.Lin1(torch.flatten(y, start_dim=1)))
         y = self.lr2(self.Lin2(y))
         y = self.lr3(self.Lin3(y))
@@ -245,7 +245,7 @@ def train_generalized_CNN():
         w3=32,#
         w4=32,#
         w5=16,#
-        w6=10*10,
+        w6=10*10 + 1,
         w7=64,
         w8=32,
         w9=16,
